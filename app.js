dcc549891f2d56909008f18e9c930c77753b24b8
Nina Herrera <ninaeherrera@gmail.com>
2023-02-22 20:30:54 -0500

    created constants in the app file to interact with the html elements

---
+++ app.js
@@ -0,0 +1,15 @@
+const thankYouContainer = document.querySelectorAll('thankyou-container');
+const ratingContainer = document.querySelectorAll('rating-container');
+const ratingList = document.querySelectorAll('.number');
+const button = document.querySelectorAll('btn-submit');
+const selection = document.querySelectorAll()
+
+
+
+// const ratingCardDiv = document.querySelector(".rating-container")
+// const thankYouCard = document.querySelector(".thank-you-container")
+// const ratingList = document.querySelectorAll(".number");
+// const button = document.querySelector(".submit");
+// const selected = document.querySelector(".selected");
+// let value = document.querySelector(".rating");
+// let selectedNumber = undefined;
